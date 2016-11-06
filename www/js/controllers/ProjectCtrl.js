f68ded2a1995097f7b0356990d7ebb0bdc869615
Samuel Martins <sam.martins@outlook.com>
2016-11-06 13:01:40 -0200

    Add project to database

--- www/js/controllers/ProjectCtrl.js
+++ www/js/controllers/ProjectCtrl.js
@@ -1,6 +1,7 @@
 angular.module('TMApp')
   .controller('ProjectCtrl', function($scope, apiService, projectService){
-
+    //On enter view
+    
     //Request on project service
     projectService.getProjects()
       .then(function(res){
@@ -9,4 +10,14 @@ angular.module('TMApp')
       }, function(err){
         console.log(err);
       });
+
+    //New project
+    $scope.projectData = {};
+    $scope.statusMsg = '';
+    $scope.addProject = function(){
+      projectService.addProject($scope.projectData)
+        .then(function(){
+          $scope.statusMsg = 'Projeto adicionado!';
+        });
+    }
   });
--- www/js/services/ProjectService.js
+++ www/js/services/ProjectService.js
@@ -3,6 +3,10 @@ angular.module('TMApp')
     return {
       getProjects: function(){
         return $http.get(apiService.url + '/project/all');
+      },
+
+      addProject: function(projectData){
+        return $http.post(apiService.url + '/project/add', projectData);
       }
     }
   });
