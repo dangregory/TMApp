37aa42e521101ec650ebf0d23e0fa1493c7e733c
Daniel Gregory <dangregory@hotmail.com>
2016-11-06 18:14:29 -0200

    Minor changes
    -Added controllers on index.html.
    -Added routes on app.js.
    -Added menu entries.

--- www/js/app.js
+++ www/js/app.js
@@ -7,50 +7,90 @@
 angular.module('TMApp', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'MainCtrl'
-  })
+        .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'MainCtrl'
+    })
 
-  .state('app.projects', {
-    url: '/projects',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/projects.html',
-        controller: 'ProjectCtrl'
-      }
-    }
-  })
+    .state('app.tasks', {
+        url: '/tasks',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/tasks.html',
+                controller: 'TasksCtrl'
+            }
+        }
+    })
 
-  .state('app.new-project', {
-    url: '/new-project',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/add-project.html',
-        controller: 'ProjectCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/projects');
+    .state('app.task', {
+        url: "/tasks/:taskId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/task.html",
+                controller: 'TaskCtrl'
+            }
+        }
+    })
+
+    .state('app.new-task', {
+        url: '/new-task',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/add-task.html',
+                controller: 'TaskCtrl'
+            }
+        }
+    })
+
+    .state('app.projects', {
+        url: '/projects',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/projects.html',
+                controller: 'ProjectsCtrl'
+            }
+        }
+    })
+
+    .state('app.project', {
+        url: "/projects/:projectId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/project.html",
+                controller: 'ProjectCtrl'
+            }
+        }
+    })
+
+    .state('app.new-project', {
+        url: '/new-project',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/add-project.html',
+                controller: 'ProjectsCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/projects');
 });
